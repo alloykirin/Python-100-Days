92e5114856eae51365ea3e658d2e8961536b048f
alloykirin <alloykirin@users.noreply.github.com>
2019-09-03 00:28:09 -0400

    d04study

---
+++ lxt/p04_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+x = int('input x: ')
+y = int('input y: ')
+
+if x > y:
+    x, y = y, x
+
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print('%d和%d的最大公约数等于： %d' % (x, y, factor))
+        print('%d和%d的最小公倍数等于： %d' % (x, y, x * y / factor))
+        break
+        
